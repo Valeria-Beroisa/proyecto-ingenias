74d1698f64c202363213c009e91234c3146e5aab
Vale Beroisa <valee.beroisa@gmail.com>
2022-10-12 22:52:35 -0300

    se agrego alert en js

--- js/main.js
+++ js/main.js
@@ -3,7 +3,12 @@ alert("BIENVENID@S")
 function saludar() {
   let nombre = prompt("Ingrese su nombre");
   console.log("hola desde al funcion saludar ():" + nombre);
+  alert("Gracias "+ nombre +" "+ " por visitarnos!!");
 }
+
+
 saludar();
+
+
  
 
